6dcf398555d7c337789b49017400165114040de9
Monaco F. J <monaco@usp.br>
2020-05-12 20:50:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Programming exercise
+
+ This small project is meant as a  a companion programming exercise for 
+ an introductory course on C programming.
+ 
+
+## Directions
+
+ Complete the source code so that the program compute the simple arithmetic 
+ mean of the values stored in the input variables. 
+
+ Upon execution, the program output shall comprise a real value with at 
+ least two decimal places.
+
+ Please, do not include anything else but the raw value in the output.
+
+ Make sure your program compiles successfully with no errors or warnings.
+ 
+ ## Reminders
+ 
+ Please, edit the fields between `<...>` in the comments at the top of file 
+ `main.c` to accordingly (give your project a cool name). 
+ 
+ Fill in file `AUTHORS` with your personal information.
+
+ ## Tips
+ 
+ Before submitting your work, make sure your compiler does not issue any 
+ error or warning messages, as well as it does produce the right output. 
+ 
+ After submission, check the automatic test results to make sure
+ everything works as expected. If not, make changes and resubmit 
+ as many times as you need.
+ 
+ Optionally, you are welcome to edit this file entirely to customize it 
+ to your project.
---
+++ main.c
@@ -0,0 +1,44 @@
+/* main.c - Calculate the average of three numbers.
+
+   Copyright (c) <year>, <author(s)-name>  <email(s)>
+   
+   This file is part of <project>
+   
+   <project> is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+
+/* This function returns the arithmentic mean of a, b and c.*/
+
+float average(float a, float b, float c)
+{
+  return 0;
+}
+
+/* This program outputs the average of three values. */
+
+int main()
+{
+  float v1, v2, v3;   /* Input variables. */ 
+  float result;       /* Output variable. */
+
+  v1 = 10;
+  v2 = 20;
+  v3 = 60; 
+  
+  result = average (v1, v2, v3);
+
+  return 0 ;
+}
